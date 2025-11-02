3bd528dce234f4768d63719a18e3f93bff99727a
Deterous <138427222+Deterous@users.noreply.github.com>
2025-11-02 15:31:52 +0900

    debug

--- SabreTools.Serialization/Readers/CDROMVolume.cs
+++ SabreTools.Serialization/Readers/CDROMVolume.cs
@@ -27,8 +27,8 @@ namespace SabreTools.Serialization.Readers
             if (SectorSize * (Constants.SystemAreaSectors + 2) > data.Length - data.Position)
                 return null;
 
-            try
-            {
+            // try
+            // {
                 // Create a new Volume to fill
                 var volume = new Volume();
 
@@ -37,19 +37,19 @@ namespace SabreTools.Serialization.Readers
 
                 // Read the set of Volume Descriptors
                 var vdSet = ParseCDROMVolumeDescriptorSet(data);
-                if (vdSet == null || vdSet.Length == 0)
-                    return null;
+                // if (vdSet == null || vdSet.Length == 0)
+                //     return null;
                 
                 volume.PathTableGroups = [];
                 volume.DirectoryDescriptors = [];
 
                 return volume;
-            }
-            catch
-            {
-                // Ignore the actual error
-                return null;
-            }
+            // }
+            // catch
+            // {
+            //     // Ignore the actual error
+            //     return null;
+            // }
         }
 
         /// <summary>
