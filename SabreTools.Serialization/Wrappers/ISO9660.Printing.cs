53f6ba479b41edc0cdc1ddf19ab396db48904dcb
Deterous <138427222+Deterous@users.noreply.github.com>
2025-11-04 13:14:51 +0900

    systemArea

--- SabreTools.Serialization/Wrappers/ISO9660.Printing.cs
+++ SabreTools.Serialization/Wrappers/ISO9660.Printing.cs
@@ -32,9 +32,9 @@ namespace SabreTools.Serialization.Wrappers
 
         protected static void Print(StringBuilder builder, byte[]? systemArea)
         {
-            if (Model.SystemArea == null || Model.SystemArea.Length == 0)
-                builder.AppendLine(Model.SystemArea, "  System Area");
-            else if (Array.TrueForAll(Model.SystemArea, b => b == 0))
+            if (systemArea == null || systemArea.Length == 0)
+                builder.AppendLine(systemArea, "  System Area");
+            else if (Array.TrueForAll(systemArea, b => b == 0))
                 builder.AppendLine("Zeroed", "  System Area");
             else
                 builder.AppendLine("Not Zeroed", "  System Area");
