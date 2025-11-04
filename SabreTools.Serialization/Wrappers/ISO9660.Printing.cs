5471d6a6f53059fa3836fdddc89d345ee2bf61fd
Deterous <138427222+Deterous@users.noreply.github.com>
2025-11-04 09:41:41 +0900

    Inherit ISO9660 (#4)

--- SabreTools.Serialization/Wrappers/ISO9660.Printing.cs
+++ SabreTools.Serialization/Wrappers/ISO9660.Printing.cs
@@ -14,19 +14,13 @@ namespace SabreTools.Serialization.Wrappers
 #endif
 
         /// <inheritdoc/>
-        public void PrintInformation(StringBuilder builder)
+        public virtual void PrintInformation(StringBuilder builder)
         {
             builder.AppendLine("ISO 9660 Information:");
             builder.AppendLine("-------------------------");
             builder.AppendLine();
 
-            if (Model.SystemArea == null || Model.SystemArea.Length == 0)
-                builder.AppendLine(Model.SystemArea, "System Area");
-            else if (Array.TrueForAll(Model.SystemArea, b => b == 0))
-                builder.AppendLine("Zeroed", "System Area");
-            else
-                builder.AppendLine("Not Zeroed", "System Area");
-            builder.AppendLine();
+            Print(builder, Model.SystemArea);
 
             Print(builder, Model.VolumeDescriptorSet);
 
@@ -34,6 +28,17 @@ namespace SabreTools.Serialization.Wrappers
             Encoding encoding = Encoding.UTF8;
             Print(builder, Model.PathTableGroups, encoding);
             Print(builder, Model.DirectoryDescriptors, encoding);
+        }
+
+        protected static void Print(StringBuilder builder, byte[]? systemArea)
+        {
+            if (Model.SystemArea == null || Model.SystemArea.Length == 0)
+                builder.AppendLine(Model.SystemArea, "  System Area");
+            else if (Array.TrueForAll(Model.SystemArea, b => b == 0))
+                builder.AppendLine("Zeroed", "  System Area");
+            else
+                builder.AppendLine("Not Zeroed", "  System Area");
+            builder.AppendLine();
         }
 
         #region Volume Descriptors
