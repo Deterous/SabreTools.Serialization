c3fc9d578d515042c9d1c251ac46ab47a9f0ae14
Deterous <138427222+Deterous@users.noreply.github.com>
2026-03-28 22:34:00 +0900

    revert CI changes

--- publish-win.ps1
+++ publish-win.ps1
@@ -1,221 +1,242 @@
+#!/bin/bash
+
 # This batch file assumes the following:
 # - .NET 10.0 (or newer) SDK is installed and in PATH
-# - 7-zip commandline (7z.exe) is installed and in PATH
-# - Git for Windows is installed and in PATH
+# - zip is installed and in PATH
+# - Git is installed and in PATH
 #
 # If any of these are not satisfied, the operation may fail
 # in an unpredictable way and result in an incomplete output.
 
 # Optional parameters
-param(
-    [Parameter(Mandatory = $false)]
-    [Alias("UseAll")]
-    [switch]$USE_ALL,
-
-    [Parameter(Mandatory = $false)]
-    [Alias("IncludeDebug")]
-    [switch]$INCLUDE_DEBUG,
-
-    [Parameter(Mandatory = $false)]
-    [Alias("IncludeUnpublished")]
-    [switch]$INCLUDE_UNPUBLISHED,
-
-    [Parameter(Mandatory = $false)]
-    [Alias("NoBuild")]
-    [switch]$NO_BUILD,
-
-    [Parameter(Mandatory = $false)]
-    [Alias("NoArchive")]
-    [switch]$NO_ARCHIVE
-)
+USE_ALL=false
+INCLUDE_DEBUG=false
+INCLUDE_UNPUBLISHED=false
+NO_BUILD=false
+NO_ARCHIVE=false
+while getopts "udpba" OPTION; do
+    case $OPTION in
+    u)
+        USE_ALL=true
+        ;;
+    d)
+        INCLUDE_DEBUG=true
+        ;;
+    p)
+        INCLUDE_UNPUBLISHED=true
+        ;;
+    b)
+        NO_BUILD=true
+        ;;
+    a)
+        NO_ARCHIVE=true
+        ;;
+    *)
+        echo "Invalid option provided"
+        exit 1
+        ;;
+    esac
+done
 
 # Set the current directory as a variable
-$BUILD_FOLDER = $PSScriptRoot
+BUILD_FOLDER=$PWD
 
 # Set the current commit hash
-$COMMIT = git log --pretty=format:"%H" -1
+COMMIT=$(git log --pretty=%H -1)
 
 # Output the selected options
-Write-Host "Selected Options:"
-Write-Host "  Use all frameworks (-UseAll)                        $USE_ALL"
-Write-Host "  Include debug builds (-IncludeDebug)                $INCLUDE_DEBUG"
-Write-Host "  Include unpublished packages (-IncludeUnpublished)  $INCLUDE_DEBUG"
-Write-Host "  No build (-NoBuild)                                 $NO_BUILD"
-Write-Host "  No archive (-NoArchive)                             $NO_ARCHIVE"
-Write-Host " "
+echo "Selected Options:"
+echo "  Use all frameworks (-u)               $USE_ALL"
+echo "  Include debug builds (-d)             $INCLUDE_DEBUG"
+echo "  Include unpublished packages (-p)     $INCLUDE_UNPUBLISHED"
+echo "  No build (-b)                         $NO_BUILD"
+echo "  No archive (-a)                       $NO_ARCHIVE"
+echo " "
 
 # Create the build matrix arrays
-$FRAMEWORKS = @('net10.0')
-$RUNTIMES = @('win-x86', 'win-x64', 'win-arm64', 'linux-x64', 'linux-arm64', 'osx-x64', 'osx-arm64')
+FRAMEWORKS=("net10.0")
+RUNTIMES=("win-x86" "win-x64" "win-arm64" "linux-x64" "linux-arm64" "osx-x64" "osx-arm64")
 
 # Use expanded lists, if requested
-if ($USE_ALL.IsPresent) {
-    $FRAMEWORKS = @('net20', 'net35', 'net40', 'net452', 'net462', 'net472', 'net48', 'netcoreapp3.1', 'net5.0', 'net6.0', 'net7.0', 'net8.0', 'net9.0', 'net10.0')
-}
+if [ $USE_ALL = true ]; then
+    FRAMEWORKS=("net20" "net35" "net40" "net452" "net462" "net472" "net48" "netcoreapp3.1" "net5.0" "net6.0" "net7.0" "net8.0" "net9.0" "net10.0")
+fi
 
 # Create the filter arrays
-$SINGLE_FILE_CAPABLE = @('net5.0', 'net6.0', 'net7.0', 'net8.0', 'net9.0', 'net10.0')
-$VALID_APPLE_FRAMEWORKS = @('net6.0', 'net7.0', 'net8.0', 'net9.0', 'net10.0')
-$VALID_CROSS_PLATFORM_FRAMEWORKS = @('netcoreapp3.1', 'net5.0', 'net6.0', 'net7.0', 'net8.0', 'net9.0', 'net10.0')
-$VALID_CROSS_PLATFORM_RUNTIMES = @('win-arm64', 'linux-x64', 'linux-arm64', 'osx-x64', 'osx-arm64')
+SINGLE_FILE_CAPABLE=("net5.0" "net6.0" "net7.0" "net8.0" "net9.0" "net10.0")
+VALID_APPLE_FRAMEWORKS=("net6.0" "net7.0" "net8.0" "net9.0" "net10.0")
+VALID_CROSS_PLATFORM_FRAMEWORKS=("netcoreapp3.1" "net5.0" "net6.0" "net7.0" "net8.0" "net9.0" "net10.0")
+VALID_CROSS_PLATFORM_RUNTIMES=("win-arm64" "linux-x64" "linux-arm64" "osx-x64" "osx-arm64")
 
 # Only build if requested
-if (!$NO_BUILD.IsPresent) {
+if [ $NO_BUILD = false ]; then
     # Restore Nuget packages for all builds
-    Write-Host "Restoring Nuget packages"
+    echo "Restoring Nuget packages"
     dotnet restore
 
     # Create published Nuget Package
-    dotnet pack SabreTools.Serialization\SabreTools.Serialization.csproj --output $BUILD_FOLDER
+    dotnet pack SabreTools.Serialization/SabreTools.Serialization.csproj --output $BUILD_FOLDER
 
     # Create unpublished Nuget Packages
-    if ($INCLUDE_UNPUBLISHED.IsPresent) {
-        dotnet pack SabreTools.Data.Extensions\SabreTools.Data.Extensions.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.Data.Models\SabreTools.Data.Models.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.Metadata\SabreTools.Metadata.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.Metadata.DatFiles\SabreTools.Metadata.DatFiles.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.Metadata.DatItems\SabreTools.Metadata.DatItems.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.Metadata.Filter\SabreTools.Metadata.Filter.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.ObjectIdentifier\SabreTools.ObjectIdentifier.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.Serialization.CrossModel\SabreTools.Serialization.CrossModel.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.Serialization.Readers\SabreTools.Serialization.Readers.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.Serialization.Writers\SabreTools.Serialization.Writers.csproj --output $BUILD_FOLDER
-        dotnet pack SabreTools.Wrappers\SabreTools.Wrappers.csproj --output $BUILD_FOLDER
-    }
+    if [ $INCLUDE_UNPUBLISHED = true ]; then
+        dotnet pack SabreTools.Data.Extensions/SabreTools.Data.Extensions.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.Data.Models/SabreTools.Data.Models.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.Metadata/SabreTools.Metadata.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.Metadata.DatFiles/SabreTools.Metadata.DatFiles.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.Metadata.DatItems/SabreTools.Metadata.DatItems.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.Metadata.Filter/SabreTools.Metadata.Filter.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.ObjectIdentifier/SabreTools.ObjectIdentifier.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.Serialization.CrossModel/SabreTools.Serialization.CrossModel.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.Serialization.Readers/SabreTools.Serialization.Readers.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.Serialization.Writers/SabreTools.Serialization.Writers.csproj --output $BUILD_FOLDER
+        dotnet pack SabreTools.Wrappers/SabreTools.Wrappers.csproj --output $BUILD_FOLDER
+    fi
 
     # Build ExtractionTool
-    foreach ($FRAMEWORK in $FRAMEWORKS) {
-        foreach ($RUNTIME in $RUNTIMES) {
+    for FRAMEWORK in "${FRAMEWORKS[@]}"; do
+        for RUNTIME in "${RUNTIMES[@]}"; do
             # Output the current build
-            Write-Host "===== Build ExtractionTool - $FRAMEWORK, $RUNTIME ====="
+            echo "===== Build ExtractionTool - $FRAMEWORK, $RUNTIME ====="
 
             # If we have an invalid combination of framework and runtime
-            if ($VALID_CROSS_PLATFORM_FRAMEWORKS -notcontains $FRAMEWORK -and $VALID_CROSS_PLATFORM_RUNTIMES -contains $RUNTIME) {
-                Write-Host "Skipped due to invalid combination"
-                continue
-            }
+            if [[ ! $(echo ${VALID_CROSS_PLATFORM_FRAMEWORKS[@]} | fgrep -w $FRAMEWORK) ]]; then
+                if [[ $(echo ${VALID_CROSS_PLATFORM_RUNTIMES[@]} | fgrep -w $RUNTIME) ]]; then
+                    echo "Skipped due to invalid combination"
+                    continue
+                fi
+            fi
 
             # If we have Apple silicon but an unsupported framework
-            if ($VALID_APPLE_FRAMEWORKS -notcontains $FRAMEWORK -and $RUNTIME -eq 'osx-arm64') {
-                Write-Host "Skipped due to no Apple Silicon support"
-                continue
-            }
+            if [[ ! $(echo ${VALID_APPLE_FRAMEWORKS[@]} | fgrep -w $FRAMEWORK) ]]; then
+                if [ $RUNTIME = "osx-arm64" ]; then
+                    echo "Skipped due to no Apple Silicon support"
+                    continue
+                fi
+            fi
 
             # Only .NET 5 and above can publish to a single file
-            if ($SINGLE_FILE_CAPABLE -contains $FRAMEWORK) {
+            if [[ $(echo ${SINGLE_FILE_CAPABLE[@]} | fgrep -w $FRAMEWORK) ]]; then
                 # Only include Debug if set
-                if ($INCLUDE_DEBUG.IsPresent) {
-                    dotnet publish ExtractionTool\ExtractionTool.csproj -f $FRAMEWORK -r $RUNTIME -c Debug --self-contained true --version-suffix $COMMIT -p:PublishSingleFile=true
-                }
-                dotnet publish ExtractionTool\ExtractionTool.csproj -f $FRAMEWORK -r $RUNTIME -c Release --self-contained true --version-suffix $COMMIT -p:PublishSingleFile=true -p:DebugType=None -p:DebugSymbols=false
-            }
-            else {
+                if [ $INCLUDE_DEBUG = true ]; then
+                    dotnet publish ExtractionTool/ExtractionTool.csproj -f $FRAMEWORK -r $RUNTIME -c Debug --self-contained true --version-suffix $COMMIT -p:PublishSingleFile=true
+                fi
+                dotnet publish ExtractionTool/ExtractionTool.csproj -f $FRAMEWORK -r $RUNTIME -c Release --self-contained true --version-suffix $COMMIT -p:PublishSingleFile=true -p:DebugType=None -p:DebugSymbols=false
+            else
                 # Only include Debug if set
-                if ($INCLUDE_DEBUG.IsPresent) {
-                    dotnet publish ExtractionTool\ExtractionTool.csproj -f $FRAMEWORK -r $RUNTIME -c Debug --self-contained true --version-suffix $COMMIT
-                }
-                dotnet publish ExtractionTool\ExtractionTool.csproj -f $FRAMEWORK -r $RUNTIME -c Release --self-contained true --version-suffix $COMMIT -p:DebugType=None -p:DebugSymbols=false
-            }
-        }
-    }
+                if [ $INCLUDE_DEBUG = true ]; then
+                    dotnet publish ExtractionTool/ExtractionTool.csproj -f $FRAMEWORK -r $RUNTIME -c Debug --self-contained true --version-suffix $COMMIT
+                fi
+                dotnet publish ExtractionTool/ExtractionTool.csproj -f $FRAMEWORK -r $RUNTIME -c Release --self-contained true --version-suffix $COMMIT -p:DebugType=None -p:DebugSymbols=false
+            fi
+        done
+    done
 
     # Build InfoPrint
-    foreach ($FRAMEWORK in $FRAMEWORKS) {
-        foreach ($RUNTIME in $RUNTIMES) {
+    for FRAMEWORK in "${FRAMEWORKS[@]}"; do
+        for RUNTIME in "${RUNTIMES[@]}"; do
             # Output the current build
-            Write-Host "===== Build InfoPrint - $FRAMEWORK, $RUNTIME ====="
+            echo "===== Build InfoPrint - $FRAMEWORK, $RUNTIME ====="
 
             # If we have an invalid combination of framework and runtime
-            if ($VALID_CROSS_PLATFORM_FRAMEWORKS -notcontains $FRAMEWORK -and $VALID_CROSS_PLATFORM_RUNTIMES -contains $RUNTIME) {
-                Write-Host "Skipped due to invalid combination"
-                continue
-            }
+            if [[ ! $(echo ${VALID_CROSS_PLATFORM_FRAMEWORKS[@]} | fgrep -w $FRAMEWORK) ]]; then
+                if [[ $(echo ${VALID_CROSS_PLATFORM_RUNTIMES[@]} | fgrep -w $RUNTIME) ]]; then
+                    echo "Skipped due to invalid combination"
+                    continue
+                fi
+            fi
 
             # If we have Apple silicon but an unsupported framework
-            if ($VALID_APPLE_FRAMEWORKS -notcontains $FRAMEWORK -and $RUNTIME -eq 'osx-arm64') {
-                Write-Host "Skipped due to no Apple Silicon support"
-                continue
-            }
+            if [[ ! $(echo ${VALID_APPLE_FRAMEWORKS[@]} | fgrep -w $FRAMEWORK) ]]; then
+                if [ $RUNTIME = "osx-arm64" ]; then
+                    echo "Skipped due to no Apple Silicon support"
+                    continue
+                fi
+            fi
 
             # Only .NET 5 and above can publish to a single file
-            if ($SINGLE_FILE_CAPABLE -contains $FRAMEWORK) {
+            if [[ $(echo ${SINGLE_FILE_CAPABLE[@]} | fgrep -w $FRAMEWORK) ]]; then
                 # Only include Debug if set
-                if ($INCLUDE_DEBUG.IsPresent) {
-                    dotnet publish InfoPrint\InfoPrint.csproj -f $FRAMEWORK -r $RUNTIME -c Debug --self-contained true --version-suffix $COMMIT -p:PublishSingleFile=true
-                }
-                dotnet publish InfoPrint\InfoPrint.csproj -f $FRAMEWORK -r $RUNTIME -c Release --self-contained true --version-suffix $COMMIT -p:PublishSingleFile=true -p:DebugType=None -p:DebugSymbols=false
-            }
-            else {
+                if [ $INCLUDE_DEBUG = true ]; then
+                    dotnet publish InfoPrint/InfoPrint.csproj -f $FRAMEWORK -r $RUNTIME -c Debug --self-contained true --version-suffix $COMMIT -p:PublishSingleFile=true
+                fi
+                dotnet publish InfoPrint/InfoPrint.csproj -f $FRAMEWORK -r $RUNTIME -c Release --self-contained true --version-suffix $COMMIT -p:PublishSingleFile=true -p:DebugType=None -p:DebugSymbols=false
+            else
                 # Only include Debug if set
-                if ($INCLUDE_DEBUG.IsPresent) {
-                    dotnet publish InfoPrint\InfoPrint.csproj -f $FRAMEWORK -r $RUNTIME -c Debug --self-contained true --version-suffix $COMMIT
-                }
-                dotnet publish InfoPrint\InfoPrint.csproj -f $FRAMEWORK -r $RUNTIME -c Release --self-contained true --version-suffix $COMMIT -p:DebugType=None -p:DebugSymbols=false
-            }
-        }
-    }
-}
+                if [ $INCLUDE_DEBUG = true ]; then
+                    dotnet publish InfoPrint/InfoPrint.csproj -f $FRAMEWORK -r $RUNTIME -c Debug --self-contained true --version-suffix $COMMIT
+                fi
+                dotnet publish InfoPrint/InfoPrint.csproj -f $FRAMEWORK -r $RUNTIME -c Release --self-contained true --version-suffix $COMMIT -p:DebugType=None -p:DebugSymbols=false
+            fi
+        done
+    done
+fi
 
 # Only create archives if requested
-if (!$NO_ARCHIVE.IsPresent) {
+if [ $NO_ARCHIVE = false ]; then
     # Create ExtractionTool archives
-    foreach ($FRAMEWORK in $FRAMEWORKS) {
-        foreach ($RUNTIME in $RUNTIMES) {
+    for FRAMEWORK in "${FRAMEWORKS[@]}"; do
+        for RUNTIME in "${RUNTIMES[@]}"; do
             # Output the current build
-            Write-Host "===== Archive ExtractionTool - $FRAMEWORK, $RUNTIME ====="
+            echo "===== Archive ExtractionTool - $FRAMEWORK, $RUNTIME ====="
 
             # If we have an invalid combination of framework and runtime
-            if ($VALID_CROSS_PLATFORM_FRAMEWORKS -notcontains $FRAMEWORK -and $VALID_CROSS_PLATFORM_RUNTIMES -contains $RUNTIME) {
-                Write-Host "Skipped due to invalid combination"
-                continue
-            }
+            if [[ ! $(echo ${VALID_CROSS_PLATFORM_FRAMEWORKS[@]} | fgrep -w $FRAMEWORK) ]]; then
+                if [[ $(echo ${VALID_CROSS_PLATFORM_RUNTIMES[@]} | fgrep -w $RUNTIME) ]]; then
+                    echo "Skipped due to invalid combination"
+                    continue
+                fi
+            fi
 
             # If we have Apple silicon but an unsupported framework
-            if ($VALID_APPLE_FRAMEWORKS -notcontains $FRAMEWORK -and $RUNTIME -eq 'osx-arm64') {
-                Write-Host "Skipped due to no Apple Silicon support"
-                continue
-            }
+            if [[ ! $(echo ${VALID_APPLE_FRAMEWORKS[@]} | fgrep -w $FRAMEWORK) ]]; then
+                if [ $RUNTIME = "osx-arm64" ]; then
+                    echo "Skipped due to no Apple Silicon support"
+                    continue
+                fi
+            fi
 
             # Only include Debug if set
-            if ($INCLUDE_DEBUG.IsPresent) {
-                Set-Location -Path $BUILD_FOLDER\ExtractionTool\bin\Debug\${FRAMEWORK}\${RUNTIME}\publish\
-                7z a -tzip $BUILD_FOLDER\ExtractionTool_${FRAMEWORK}_${RUNTIME}_debug.zip *
-            }
-        
-            Set-Location -Path $BUILD_FOLDER\ExtractionTool\bin\Release\${FRAMEWORK}\${RUNTIME}\publish\
-            7z a -tzip $BUILD_FOLDER\ExtractionTool_${FRAMEWORK}_${RUNTIME}_release.zip *
-        }
-    }
+            if [ $INCLUDE_DEBUG = true ]; then
+                cd $BUILD_FOLDER/ExtractionTool/bin/Debug/${FRAMEWORK}/${RUNTIME}/publish/
+                zip -r $BUILD_FOLDER/ExtractionTool_${FRAMEWORK}_${RUNTIME}_debug.zip .
+            fi
+            cd $BUILD_FOLDER/ExtractionTool/bin/Release/${FRAMEWORK}/${RUNTIME}/publish/
+            zip -r $BUILD_FOLDER/ExtractionTool_${FRAMEWORK}_${RUNTIME}_release.zip .
+        done
+    done
 
     # Create InfoPrint archives
-    foreach ($FRAMEWORK in $FRAMEWORKS) {
-        foreach ($RUNTIME in $RUNTIMES) {
+    for FRAMEWORK in "${FRAMEWORKS[@]}"; do
+        for RUNTIME in "${RUNTIMES[@]}"; do
             # Output the current build
-            Write-Host "===== Archive InfoPrint - $FRAMEWORK, $RUNTIME ====="
+            echo "===== Archive InfoPrint - $FRAMEWORK, $RUNTIME ====="
 
             # If we have an invalid combination of framework and runtime
-            if ($VALID_CROSS_PLATFORM_FRAMEWORKS -notcontains $FRAMEWORK -and $VALID_CROSS_PLATFORM_RUNTIMES -contains $RUNTIME) {
-                Write-Host "Skipped due to invalid combination"
-                continue
-            }
+            if [[ ! $(echo ${VALID_CROSS_PLATFORM_FRAMEWORKS[@]} | fgrep -w $FRAMEWORK) ]]; then
+                if [[ $(echo ${VALID_CROSS_PLATFORM_RUNTIMES[@]} | fgrep -w $RUNTIME) ]]; then
+                    echo "Skipped due to invalid combination"
+                    continue
+                fi
+            fi
 
             # If we have Apple silicon but an unsupported framework
-            if ($VALID_APPLE_FRAMEWORKS -notcontains $FRAMEWORK -and $RUNTIME -eq 'osx-arm64') {
-                Write-Host "Skipped due to no Apple Silicon support"
-                continue
-            }
+            if [[ ! $(echo ${VALID_APPLE_FRAMEWORKS[@]} | fgrep -w $FRAMEWORK) ]]; then
+                if [ $RUNTIME = "osx-arm64" ]; then
+                    echo "Skipped due to no Apple Silicon support"
+                    continue
+                fi
+            fi
 
             # Only include Debug if set
-            if ($INCLUDE_DEBUG.IsPresent) {
-                Set-Location -Path $BUILD_FOLDER\InfoPrint\bin\Debug\${FRAMEWORK}\${RUNTIME}\publish\
-                7z a -tzip $BUILD_FOLDER\InfoPrint_${FRAMEWORK}_${RUNTIME}_debug.zip *
-            }
-        
-            Set-Location -Path $BUILD_FOLDER\InfoPrint\bin\Release\${FRAMEWORK}\${RUNTIME}\publish\
-            7z a -tzip $BUILD_FOLDER\InfoPrint_${FRAMEWORK}_${RUNTIME}_release.zip *
-        }
-    }
+            if [ $INCLUDE_DEBUG = true ]; then
+                cd $BUILD_FOLDER/InfoPrint/bin/Debug/${FRAMEWORK}/${RUNTIME}/publish/
+                zip -r $BUILD_FOLDER/InfoPrint_${FRAMEWORK}_${RUNTIME}_debug.zip .
+            fi
+            cd $BUILD_FOLDER/InfoPrint/bin/Release/${FRAMEWORK}/${RUNTIME}/publish/
+            zip -r $BUILD_FOLDER/InfoPrint_${FRAMEWORK}_${RUNTIME}_release.zip .
+        done
+    done
 
     # Reset the directory
-    Set-Location -Path $PSScriptRoot
-}
+    cd $BUILD_FOLDER
+fi
