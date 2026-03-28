ed95eb1a3c882c7fec7c195015846fef4545edaf
Deterous <138427222+Deterous@users.noreply.github.com>
2026-03-28 11:28:57 +0900

    long

--- SabreTools.Serialization.Readers/XDVDFS.cs
+++ SabreTools.Serialization.Readers/XDVDFS.cs
@@ -232,7 +232,7 @@ namespace SabreTools.Serialization.Readers
             obj.FileFlags = (FileFlags)data.ReadByteValue();
             obj.FilenameLength = data.ReadByteValue();
             obj.Filename = data.ReadBytes(obj.FilenameLength);
-            int remainder = data.Position % 4;
+            long remainder = data.Position % 4;
             if (remainder > 0)
                 obj.Padding = data.ReadBytes(remainder);
 
