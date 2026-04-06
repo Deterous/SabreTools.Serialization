00795b19c961878141265fb3b28db430d8d6771c
Deterous <138427222+Deterous@users.noreply.github.com>
2026-04-06 11:42:55 +0900

    More table entry IDs

--- SabreTools.Data.Models/XenonExecutable/TableEntry.cs
+++ SabreTools.Data.Models/XenonExecutable/TableEntry.cs
@@ -12,6 +12,16 @@ namespace SabreTools.Data.Models.XenonExecutable
         /// Known values:
         /// 0x00000011, 0x00000012, 0x00000013 (Retail games)
         /// 0x00000101, 0x00000102, 0x00000103 (Applications)
+        /// Unique IDs from XboxMCX-V.XEX (Windows Vista):
+        ///   0x00000023, 0x000000E1 (Once, one after another)
+        ///   0x00000031, 0x000000D2 (Once, one after another)
+        ///   0x00000072, 0x00000093 (Once, one after another)
+        ///   0x00000063 (Once, final entry)
+        /// Unique IDs from XboxMCX-V.XEX (Windows 7):
+        ///   0x000000D3, 0x00000031 (Once, one after another)
+        ///   0x000000A1, 0x00000062 (Once, one after another)
+        ///   0x00000072, 0x00000093 (Once, one after another)
+        ///   0x00000023 (Once, final entry)
         /// </summary>
         /// <remarks>Big-endian</remarks>
         public uint ID { get; set; } = new();
